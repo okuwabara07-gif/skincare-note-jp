08fc3c43e6bd65fac4dd727343a527ba3ae8b3e3
okuwabara07-gif <o.kuwabara07@gmail.com>
2026-04-05 16:26:35 +0900

    feat: add next-sitemap config

---
+++ next-sitemap.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://skincare-note-jp.vercel.app',
+  generateRobotsTxt: true,
+  robotsTxtOptions: {
+    additionalSitemaps: ['https://skincare-note-jp.vercel.app/sitemap.xml'],
+  },
+  changefreq: 'daily',
+  priority: 0.7,
+  sitemapSize: 5000,
+}
